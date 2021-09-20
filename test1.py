59a26c1d5bf6b6582b2e5cc000943bf6f902f54a
Ivanstein18 <ivanstein18@yandex.ru>
2021-09-20 20:36:45 +0300

    json

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 # url = "http://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
 
@@ -16,13 +17,24 @@ from bs4 import BeautifulSoup
 # with open("index.html", "w") as file:
 #     file.write(src)
 
-with open("index.html") as file:
-    src = file.read()
+# with open("index.html") as file:
+#     src = file.read()
     
-soup = BeautifulSoup(src, "lxml")
-all_products_href = soup.find_all(class_ = "mzr-tc-group-item-href")
-for item in all_products_href:
-    item_text = item.text
-    item_href = item.get("href")
-    print(f"{item_text}:{item_href}")
+# soup = BeautifulSoup(src, "lxml")
+# all_products_href = soup.find_all(class_ = "mzr-tc-group-item-href")
+
+# all_categories_dict = {}
+# for item in all_products_href:
+#     item_text = item.text
+#     item_href = "http://health-diet.ru" + item.get("href")
+    
+#     all_categories_dict[item_text] = item_href
+
+# with open("all_categories_dict.json", "w") as file:
+#     json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
+
+with open("all_categories_dict.json") as file:
+    all_categories = json.load(file)
+
+print(all_categories)
 
