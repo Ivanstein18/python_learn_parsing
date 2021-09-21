8e78f154593184db6405b695b4793e5f9d52b300
Ivanstein18 <ivanstein18@yandex.ru>
2021-09-21 23:30:41 +0300

    tibel_head

--- test1.py
+++ test1.py
@@ -4,10 +4,10 @@ import json
 
 # url = "http://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
 
-# headers = {
-# "Accept": "*/*",
-# "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
-# }
+headers = {
+"Accept": "*/*",
+"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
+}
 
 # req = requests.get(url, headers=headers)
 
@@ -36,5 +36,30 @@ import json
 with open("all_categories_dict.json") as file:
     all_categories = json.load(file)
 
-print(all_categories)
+count = 0
 
+for category_name, category_href in all_categories.items():
+
+    if count == 0:
+        rep = [",", " ", "-", "'"]
+        for item in rep:
+            if item in category_name:
+                category_name = category_name.replace(item, "_")
+        
+        
+        req = requests.get(url=category_href, headers=headers)
+        src = req.text
+
+        with open(f"date/{count}_{category_name}.html", "w") as file:
+            file.write(src)
+
+        with open(f"date/{count}_{category_name}.html") as file:
+            scr = file.read()
+        
+        soup = BeautifulSoup(scr, "lxml")
+
+        tible_head = soup.find(class_="uk-table mzr-tc-group-table uk-table-hover uk-table-striped uk-table-condensed").find("tbody").find("tr").find_all("td")
+        print(tible_head)
+        
+
+        count +=1
